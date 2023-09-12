78904ff7d327d3690730014d539cd3d093353b49
M.Asad Ali Akbar <m.asad@bitswits.com>
2023-09-12 10:55:11 +0500

    update

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . views import ProductSearch
+from . views import ProductSearch, ProductsListView
 app_name = 'product'
 
 urlpatterns = [
-    path('search/', ProductSearch.as_view(), name='product_search')
+    path('search/', ProductSearch.as_view(), name='product_search'),
+    path('', ProductsListView.as_view(), name='product_listview')
 ]
--- product/views.py
+++ product/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .utils import global_search_utils
+from .serializer import ProductSerializer,Product
 # Create your views here.
 
 class ProductSearch(APIView):
     def get(self, request):
         resp = global_search_utils(request)
         return Response(resp, status = resp['status_code'])
+
+class ProductsListView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
